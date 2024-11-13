2c2efc1e8ead7ae63c632a73be45bd6e8c470bf4
Erika Sällberg <erikasallberg@hotmail.com>
2024-11-13 19:12:23 +0100

    chore: add comment

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{
-        fontSize: 40,
+        fontSize: 40, //storlek
         fontWeight: 600,
       }}>
         Hej Examinator
